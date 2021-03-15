b357c309f205a6fb1d5c62333fae4325e84af869
ANGELLOPARR <angellop123@gmail.com>
2021-03-15 09:18:34 -0700

    update list extractor

--- list_extractor.py
+++ list_extractor.py
@@ -4,6 +4,7 @@ import sys
 import spacy
 import nltk
 import re
+import random
 from similarity.longest_common_subsequence import LongestCommonSubsequence
 lcs = LongestCommonSubsequence()
 nlp = spacy.load("en_core_web_sm")
@@ -27,6 +28,7 @@ def get_lists(wikitext_file):
     inv_lists = {v: k for k, v in lists.items()}
     return inv_lists
 
+
 # lot = list of things
 def find_closest_item(key, lot):
     most_similar, min_dist = None, None
@@ -37,17 +39,100 @@ def find_closest_item(key, lot):
 
     return most_similar
 
-def check_list_question(inquiry):
-    lists = get_lists('California_Polytechnic_State_University.wikitext')
-    doc = nlp(inquiry)
-    entities = [(entity.text, entity.label_) for entity in doc.ents]
+
+def get_president_years(presidents, p):
+    entry = find_closest_item(p, presidents)
+    years = entry.split(',')[1].strip().split('–')
+    if len(years) < 2:
+        return '1 year'
+    elif years[1] == 'present':
+        val = 2021 - int(years[0])
+    else:
+        val = int(years[-1]) - int(years[0])
+    if val > 1:
+        return str(val) + ' years'
+    else:
+        return str(val) + ' year'
+
+
+def get_president_timeline(presidents, p):
+    entry = find_closest_item(p, presidents)
+    years = entry.split(',')[1].strip().split('–')
+    if len(years) == 2:
+        return f'{years[0]} to {years[1]}'
+    else:
+        return years[0]
+
+
+def get_president(presidents, year):
+    year = int(year)
+    for data in presidents:
+        years = data.split(',')[1].strip().split('–')
+        for i in range(len(years)):
+            if years[i] == 'present':
+                years[i] = 2021
+        years = [int(y) for y in years]
+        if len(years) == 2:
+            if year >= years[0] and year <= years[1]:
+                return data
+        else:
+            if years[0] == year:
+                return data
+    return f"No president found for year {year}"
+
+
+def get_some_items(items, keyword):
+    choices = random.sample(range(0, len(items)), 3)
+    return f'Some {keyword} are {items[choices[0]]}, {items[choices[1]]}, and {items[choices[2]]}'
+    
+
+# When was ___ president?
+# How many years was ____ president? OR
+    # How long was _____ president for?
+# Who was president in ______?
+# What are the colleges at Cal Poly?
+# What are some colleges at Cal Poly?
+# What are some fraternities at Cal Poly?
+# What is _____ Engineering/Manufacturing ranked?
+# Who are some notable alumni?
+def try_list_question(lists, question_doc):
+    tokens = set([t.text.lower() for t in question_doc])
+    entities = [(entity.text, entity.label_) for entity in question_doc.ents]
     entities = [name for name, type in entities if type == 'PERSON']
-    print(find_closest_item(entities[0], lists['Directors and presidents']))
+    pres_key = 'Directors and presidents'
+    college_key = 'Colleges'
+    rank_key = 'Rankings'
+    greek_key = 'Greek life'
+    alum_key = 'Notable alumni'
+    when_pres = {'when', 'was', 'president'}
+    timeline_pres = {'how', 'president'}
+    who_pres = {'who', 'was', 'president'}
+    what = {'what', 'are', 'some'}
+    who_alum = {'who', 'are', 'some', 'alumni'}
+    if (tokens & when_pres) == when_pres and len(entities) == 1:
+        return get_president_timeline(lists[pres_key], entities[0])
+    elif (tokens & timeline_pres) == timeline_pres and len(entities) == 1:
+        return get_president_years(lists[pres_key], entities[0])
+    elif (tokens & who_pres) == who_pres and len(entities) == 0:
+        year = [t for t in question_doc if t.pos_ == 'NUM'][0].text
+        return get_president(lists[pres_key], year)
+    elif (tokens & what) == what and ('colleges' in tokens or 'schools' in tokens):
+        return get_some_items(lists[college_key], 'colleges')
+    elif (tokens & what) == what and ('fraternities' in tokens):
+        return get_some_items(lists[greek_key], 'fraternities')
+    elif (tokens & what) == what and ('alumni' in tokens):
+        return get_some_items(lists[alum_key], 'notable alumni')
+    elif (tokens & who_alum) == who_alum:
+        return get_some_items(lists[alum_key], 'notable alumni')
+    else:
+        return None
+
+    
 
 
 if __name__ == '__main__':
-    # lists = get_lists('California_Polytechnic_State_University.wikitext')
+    lists = get_lists('California_Polytechnic_State_University.wikitext')
     # print(get_lists('California_Polytechnic_State_University.wikitext'))
     # print(clean_markup('[[Robert L. Gibson|Robert “Hoot” Gibson]], [[NASA]] Astronaut<ref>{{Cite web|title=The Astronaut|url=https://magazine.calpoly.edu/fall-2020/the-astronaut/|access-date=2020-10-08|website=Cal Poly Magazine|language=en-US}}</ref>', ignore_headers=False))
     # print(list(lists['Notable alumni']))
-    check_list_question('When was ' + sys.argv[1] + ' president?')
+    print(try_list_question(lists, nlp(sys.argv[1])))
